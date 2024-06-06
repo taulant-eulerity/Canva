9fff7f1b1843e7a56e7f87615e42b345482e0345
taulantvokshiPersonal <taulantvokshi2@gmail.com>
2024-06-06 11:24:02 -0400

    changes

--- src/components/util/util.js
+++ src/components/util/util.js
@@ -1,3 +1,5 @@
+import { mergeSort } from "../pages/creative/controller/style.controller";
+
 export const getLastValue = (rgba) => {
     if (rgba) {
       return rgba.split("(").join(",").split(")").join(",").split(",").slice(1, -1);
@@ -8,6 +10,7 @@ export const getLastValue = (rgba) => {
   export function getFilter(filter) {
     return {
       filter,
-      id: "a"
+      id: "a",
+      sort: mergeSort([8,4,1,2,3,565,7,8,9,0,3,24,5,56])
     }
   }
