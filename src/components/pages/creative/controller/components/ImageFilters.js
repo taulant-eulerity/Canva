ac7cc9c19d29f0d8b3c3f32f5fa95253bfd4b35d
taulantvokshiPersonal <taulantvokshi2@gmail.com>
2024-06-06 11:11:28 -0400

    changes

--- src/components/pages/creative/controller/components/ImageFilters.js
+++ src/components/pages/creative/controller/components/ImageFilters.js
@@ -16,14 +16,6 @@ const ImageFilters = () => {
         {filterObject: new fabric.Image.filters.Technicolor(), filterName: 'Technicolor'},
         {filterObject: new fabric.Image.filters.Invert(), filterName: 'Invert'}
     ]
-
-
-    const c = function(num) {
-        return function(num2) {
-            return num + num2
-        }
-    }
-
     const handleFilter = (name) => {
         const FO = filters.find(f => f.filterName === name)
         if(!FO) return
